e8164ea28ec4e8f2cf45a4b520b899229c5d892b
hyperchessbot <hyperchessbot@gmail.com>
2021-10-03 18:12:13 +0000

    Test

---
+++ test/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <publishdemo/>
+</template>
+
+<script>
+import publishdemo from "../../publishdemo/dist/publishdemo.js"
+export default {
+  components:{
+    publishdemo
+  }
+}
+</script>
