27b44e9b95a5a2b253bb3295f0e7e6079abe9dff
hyperchessbot <hyperchessbot@gmail.com>
2021-10-03 12:34:09 +0000

    Remove props

--- publishdemo/src/components/HelloWorld.vue
+++ publishdemo/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>Message</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -32,10 +32,7 @@
 
 <script>
 export default {
-  name: 'publishdemo',
-  props: {
-    msg: String
-  }
+  name: 'publishdemo'
 }
 </script>
 
